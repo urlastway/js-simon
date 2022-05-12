60096faf423d3bf7872b394189437b666fa6ddbd
urlastway <andriy.semenchenko13@gmail.com>
2022-05-12 23:58:14 +0200

    com1

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-var pc__number = document.getElementById("pc__number")
+const pc__number = document.getElementById("pc__number")
+const user__number = document.getElementById("user__number")
+const numbers__guessed = document.getElementById("numbers__guessed")
+
 var pc__numbers = [];
 var user__numbers = [];
 var numbers__hiddens = [];
+let i = 0;
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -15,19 +19,18 @@ while (pc__numbers.length <= 4 ) {
 pc__number.innerHTML = pc__numbers;
 
 
-setTimeout(Timingout, 3000);
+setTimeout(Timingout, 30000);
 function Timingout () {
     document.getElementById("pc__number").style.display = "none";
     while(user__numbers.length <= 4){
         user__numbers.push(Number(prompt("Inserisci il numero")));
+        if(user__numbers[i] == pc__numbers[i]){
+            numbers__hiddens.push(user__numbers[i]);
+        };
+        i += 1;
     };
-    console.log(user__numbers);
-};
-
-for(let i = 0; i <= 4; i++){
-    if(user__numbers[i] == pc__numbers[i]){
-        numbers__hiddens.push(user__numbers[i]);      
-    }
+user__number.innerHTML = numbers__hiddens;
+numbers__guessed.innerHTML = numbers__hiddens.length; 
 };
 
 
